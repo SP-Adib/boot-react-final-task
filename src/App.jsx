0a21c9077f0fe60daf78aab0b94774b7528b32e8
Adib Shojaheydari <adib.ah1245@gmail.com>
2026-02-28 00:09:42 +0330

    changes versions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { DarkModeProvider } from "./Context/DarkModeContext";
-import { Toaster } from "react-hot-toast";
 import useLocalStorageState from "./hooks/useLocalStorageState";
 import AppHeader from "./ui/AppHeader";
 import MainBody from "./ui/MainBody";
@@ -9,7 +8,6 @@ function App() {
   const [products, setProducts] = useLocalStorageState("PRODUCTS", []);
   return (
     <DarkModeProvider>
-      <Toaster />
       <div className="bg-secondary-0 min-h-screen">
         <AppHeader products={products} />
         <MainBody products={products} setProducts={setProducts} />
